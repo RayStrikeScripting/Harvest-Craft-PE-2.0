416171c2a8509049a03ad1b761930cf56669a6eb
RayStrikeScripting <32301961+RayStrikeScripting@users.noreply.github.com>
2017-12-17 22:48:06 +1000

    Unless
    Deleted c:

--- dev/CROPS/peppercorn.js
+++
@@ -1,9 +0,0 @@
-CropRegistry.registerWithID("peppercorncrop","peppercorncrop","peppercorncrop",ItemID.peppercorn_seed);
-CropRegistry.fruitPush(BlockID.peppercorncrop,ItemID.peppercorn);
-Harvest.registerDroppingBlock(BlockID.peppercorncrop);
-
-PlantModel.crop(BlockID.peppercorncrop);
-
-TileEntity.registerPrototype(BlockID.peppercorncrop,cropPROTO);
-
-CropRegistry.registerSeed(ItemID.peppercorn_seed,BlockID.peppercorncrop);
